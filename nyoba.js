1417acc87bb0a23754b8e2b81a05cd20426145c4
Christianmike12 <christianmikhael93@gmail.com>
2026-04-07 11:49:11 +0700

    submit

---
+++ nyoba.js
@@ -0,0 +1,8 @@
+function tampilPesan() {
+  alert("Halo! Terima kasih sudah mengunjungi profil saya 😄");
+}
+document.getElementById("bio").innerHTML = "Saya sekarang lebih jago coding!";
+<button onclick="ubahWarna()">Ubah Warna</button>
+function ubahWarna() {
+  document.body.style.backgroundColor = "lightblue";
+}
